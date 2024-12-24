3ce3984f8cc1026f999879b837ca76078334ce33
Christina <christina.hayoung@gmail.com>
2024-12-24 00:09:05 +0000

    add evaluation for math operations

--- parsing.c
+++ parsing.c
@@ -26,6 +26,39 @@ void add_history(char* unused) {}
 #include <editline/history.h>
 #endif
 
+/* Use operator string to determine which operation to perform */
+long eval_op(long x, char* op, long y) {
+  if (strcmp(op, "+") == 0) { return x + y; }
+  if (strcmp(op, "-") == 0) { return x - y; }
+  if (strcmp(op, "*") == 0) { return x * y; }
+  if (strcmp(op, "/") == 0) { return x / y; }
+  return 0;
+}
+
+/* Evaluate the parse tree */
+long eval(mpc_ast_t* t) {
+  
+  /* If tagged as number, return directly */
+  if (strstr(t->tag, "number")) {
+    return atoi(t->contents);
+  }
+  
+  /* The first operator is always '(', the second is always an operator */
+  char* op = t->children[1]->contents;
+  
+  /* Store third child (num/expression) in x */
+  long x = eval(t->children[2]);
+  
+  /* Iterate the remaining children and combine */
+  int i = 3;
+  while (strstr(t->children[i]->tag, "expr")) {
+    x = eval_op(x, op, eval(t->children[i]));
+    i++;
+  }
+  
+  return x;
+}
+
 int main(int argc, char** argv) {
   /* Create some parsers */
   mpc_parser_t* Number = mpc_new("number");
@@ -60,7 +93,8 @@ int main(int argc, char** argv) {
     mpc_result_t r;
     if (mpc_parse("<stdin>", input, Lispy, &r)) {
       /* On success print the AST */
-      mpc_ast_print(r.output);
+      long result = eval(r.output);
+      printf("%li\n", result);
       mpc_ast_delete(r.output);
     } else {
       /* Otherwise print error */
@@ -76,4 +110,4 @@ int main(int argc, char** argv) {
   mpc_cleanup(4, Number, Operator, Expr, Lispy);
   
   return 0;
-}
+} 
